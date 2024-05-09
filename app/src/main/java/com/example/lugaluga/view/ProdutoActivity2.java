e7b6107e6002c4ccf03ba2915fde3f7779a08ca6
Maria Eduarda Leitao <marialeitao3006@gmail.com>
2024-05-09 16:09:30 -0300

    Aula dia 9/05

--- app/src/main/java/com/example/lugaluga/view/ProdutoActivity2.java
+++ app/src/main/java/com/example/lugaluga/view/ProdutoActivity2.java
@@ -1,9 +1,16 @@
 package com.example.lugaluga.view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.example.lugaluga.R;
 import com.example.lugaluga.model.Produto;
@@ -14,23 +21,57 @@ public class ProdutoActivity2 extends AppCompatActivity {
 
     private TextView nomeProduto, qtdProduto, precoProduto, descProduto;
 
+    private Button btnAlugar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto2);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         nomeProduto = findViewById(R.id.nomeProdutoTv);
         qtdProduto = findViewById(R.id.qtdProdutoTv);
         precoProduto = findViewById(R.id.precoProduto);
         descProduto = findViewById(R.id.descricaoTv);
 
+        btnAlugar = findViewById(R.id.alugaBtn);
+
 
        produto = getIntent().getExtras().getParcelable("produto");
 
        nomeProduto.setText(produto.getNome());
-       qtdProduto.setText(produto.getQuantidade());
+       qtdProduto.setText(String.valueOf(produto.getQuantidade()));
        precoProduto.setText(String.valueOf(produto.getPreco()));
        descProduto.setText(produto.getDescricao());
+
+       btnAlugar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               AlertDialog.Builder builder = new AlertDialog.Builder(ProdutoActivity2.this);
+               builder.setMessage("Deseja realmente alugar este produto?");
+               builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       Toast.makeText(getApplicationContext(), "Concluído", Toast.LENGTH_SHORT).show();
+
+                   }
+               });
+
+               builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+
+                   }
+               });
+
+
+
+               AlertDialog alertDialog = builder.create();
+               alertDialog.show();
+           }
+       });
 
     }
 }
